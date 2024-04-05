c884bd93188ccdbb40d497f1825ad30a792f4f0b
Matthew Rozak <matthew.rozak@mail.utoronto.ca>
2024-04-05 15:43:37 -0400

    add script for registering in parallel on cluster

---
+++ register_files_0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=rrg-bojana_cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=3:00:00
+#SBATCH --mem=249GB
+#SBATCH --array=0-20
+#SBATCH --cpus-per-task=64
+#SBATCH --mail-user=matthew.rozak@mail.utoronto.ca
+#SBATCH --out=/home/rozakmat/projects/rrg-bojana/rozakmat/TBI_monai_UNET/logs/register-%A_%a.out --array=0-20
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+
+source ~/projects/def-bojana/rozakmat/monai3.8/bin/activate
+python ~/projects/rrg-bojana/rozakmat/TBI_monai_UNET/Register_files.py 
